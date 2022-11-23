89baed02b341814495a6679044abe34201f199d3
Peter Moss <peter@moss.dk>
2022-11-23 19:33:11 +0100

    more stuff

--- src/app/edit/new-class/new-class.component.ts
+++ src/app/edit/new-class/new-class.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ClassService } from 'src/app/services/class.service';
+import { Dice } from 'src/app/util';
 import CharacterClass from 'src/models/CharacterClass';
 
 @Component({
@@ -12,53 +13,13 @@ export class NewClassComponent {
     name: 'New Class',
     hitDie: 6,
     features: [],
-    magicTalents: {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-      10: 0,
-      11: 0,
-      12: 0,
-      13: 0,
-      14: 0,
-      15: 0,
-      16: 0,
-      17: 0,
-      18: 0,
-      19: 0,
-      20: 0,
-    },
-    spellPoints: {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-      10: 0,
-      11: 0,
-      12: 0,
-      13: 0,
-      14: 0,
-      15: 0,
-      16: 0,
-      17: 0,
-      18: 0,
-      19: 0,
-      20: 0,
-    },
+    magicTalents: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, },
+    spellPoints: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, },
     savingThrows: [['Wisdom']],
     specializations: {},
   }
+
+  dice = Dice
 
   constructor(
     private classService: ClassService,
--- src/app/util.ts
+++ src/app/util.ts
@@ -1,3 +1,5 @@
+import Die from "src/types/Die"
+
 export const arrayToRecord = <K extends string | number | symbol,V>(arr: {key: K, value: V}[]): Record<K,V> => {
   return arr.reduce((acc, { key, value }) => {
     acc[key] = value
@@ -6,3 +8,13 @@ export const arrayToRecord = <K extends string | number | symbol,V>(arr: {key: K
 }
 
 export const BASEURL = 'http://localhost:3000'
+
+export const Dice: Record<'d4'|'d6'|'d8'|'d10'|'d12'|'d20'|'d100', Die> = {
+  d4: 4,
+  d6: 6,
+  d8: 8,
+  d10: 10,
+  d12: 12,
+  d20: 20,
+  d100: 100,
+}
